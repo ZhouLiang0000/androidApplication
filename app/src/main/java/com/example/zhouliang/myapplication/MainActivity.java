e81ed9b0a3fd8b9f3d0c108ddc0512b32319e914
zhouliang <18510971680@163.com>
2017-05-09 16:00:08 +0800

    增加aar包引用

--- app/src/main/java/com/example/zhouliang/myapplication/MainActivity.java
+++ app/src/main/java/com/example/zhouliang/myapplication/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.zhouliang.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.commutils.activity.SecondActivity;
+import com.example.commutils.utils.ToActivityUtil;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @InjectView(R.id.bt_start)
+    Button btStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+    }
+
+    @OnClick(R.id.bt_start)
+    public void onViewClicked() {
+        ToActivityUtil.toNextActivity(MainActivity.this, SecondActivity.class);
     }
 }
